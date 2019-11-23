823cac04835db70ccd610270dc3e8f64a3b1f6aa
chamincheol <54410332+chamincheol@users.noreply.github.com>
2019-11-24 00:51:02 +0900

    initiate

---
+++ app/src/main/java/com/example/photo_diary/Main/MainAppActivity.java
@@ -0,0 +1,16 @@
+package com.example.photo_diary.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.photo_diary.R;
+
+public class MainAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app);
+    }
+}
